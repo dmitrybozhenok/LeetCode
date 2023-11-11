03b04fa9bb4684580f7a6fe443c9b765ad2c554e
Dmitry Bozhenok <dmitry.bozhenok@gmail.com>
2023-11-11 22:00:38 +0000

    Add 239 sliding window maximum

---
+++ Solutions/239-sliding-window-maximum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        ret = []
+        window = []
+
+        for i in range(k):
+            if not window:
+                window.append(nums[i])
+            else:
+                while window and nums[i] > window[-1]:
+                    window.pop()
+                window.append(nums[i])
+        ret.append(window[0])
+
+        for i in range(0, len(nums) - k):
+            if nums[i] == window[0]:
+                window.pop(0)
+            while window and nums[i + k] > window[-1]:
+                window.pop()
+            window.append(nums[i + k])
+            ret.append(window[0])
+
+        return ret
+
+
+solution = Solution()
+ret = solution.maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)
